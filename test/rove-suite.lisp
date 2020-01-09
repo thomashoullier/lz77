49a8f7489dae59a59183344e9a61f2cccd5d7545
Karl <a>
2020-01-09 22:10:28 +0100

    Added two test cases for encoder on single arrays.

--- test/rove-suite.lisp
+++ test/rove-suite.lisp
@@ -5,6 +5,8 @@
 ;;;   * Add tests for encode and decode of messages in multiple parts.
 ;;;   * Add encode and decode of random data, check we get the same thing after
 ;;;     an identity operation. Include tests with fragmented messages.
+;;;   * Add test for maximum string length in encoder.
+;;;   * Add more tests for edge cases, eg. very few inputs.
 
 ;;; Decoder
 (deftest decoder
@@ -39,3 +41,34 @@
       (setf lz77-decoder (make-lz77-decoder))
       (setf decoded (decode lz77-decoder literals triplets))
       (ok (equalp decoded valid-decoded) (format nil "decoded: ~A" decoded)))))
+
+;;; Encoder
+(deftest encoder
+  ;; Case 1
+  (let ((lz77-encoder)
+        (to-encode #(0 0 0 0 4 3 2 5 4 3 2 1 5 1 5 2 3))
+        (literals)
+        (valid-literals #(4 3 2 5 1 5 1 5 2 3))
+        (triplets)
+        (valid-triplets #(#(4 1 0) #(3 4 8))))
+    (testing (format nil "case 1 ~A" to-encode)
+      (setf lz77-encoder (make-lz77-encoder))
+      (pass "LZ77 encoder instantiated.")
+      (multiple-value-setq (literals triplets)
+        (values-list (encode lz77-encoder to-encode)))
+      (ok (and (equalp literals valid-literals)
+               (equalp triplets valid-triplets))
+          (format nil "literals: ~A triplets: ~A" literals triplets))))
+  ;; Case 2
+  (let ((lz77-encoder (make-lz77-encoder))
+        (to-encode #(4 3 2 5 4 3 2 1 5 1 5 2 3))
+        (literals)
+        (valid-literals #(4 3 2 5 1 5 1 5 2 3))
+        (triplets)
+        (valid-triplets #(#(3 4 4))))
+    (testing (format nil "case 2 ~A" to-encode)
+      (multiple-value-setq (literals triplets)
+        (values-list (encode lz77-encoder to-encode)))
+      (ok (and (equalp literals valid-literals)
+               (equalp triplets valid-triplets))
+          (format nil "literals: ~A triplets: ~A" literals triplets)))))
